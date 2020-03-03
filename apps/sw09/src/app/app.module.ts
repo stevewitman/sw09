74e8ae02ec9218badc97f68668628f10be3ec637
Steve Witman <stevewitman@gmail.com>
2020-03-03 07:22:38 -0700

    Added angular material and bootstrap grid utilities

--- apps/sw09/src/app/app.module.ts
+++ apps/sw09/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@bb/material';
+import { CoreDataModule } from '@bb/core-data';
+import { UiToolbarModule } from '@bb/ui-toolbar';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    CoreDataModule,
+    UiToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
