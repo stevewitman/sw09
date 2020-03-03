a8014b90a48df5099b53637c8afc87626c076cbb
Steve Witman <stevewitman@gmail.com>
2020-03-03 07:29:24 -0700

    Added projects service and components

--- apps/sw09/src/app/app.module.ts
+++ apps/sw09/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@bb/material';
 import { CoreDataModule } from '@bb/core-data';
 import { UiToolbarModule } from '@bb/ui-toolbar';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ProjectsListComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
